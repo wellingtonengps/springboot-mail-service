96b462a3ae5d58171163905b05f094235b45a99a
Wellington Pereira <43380060+wellingtonengps@users.noreply.github.com>
2024-04-29 21:01:49 -0300

    first commit

---
+++ src/main/java/org/example/springboot_email_service/Email.java
@@ -0,0 +1,2 @@
+package org.example.springboot_email_service;public record Email() {
+}
---
+++ src/main/java/org/example/springboot_email_service/SpringbootEmailServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.springboot_email_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEmailServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootEmailServiceApplication.class, args);
+	}
+
+}
